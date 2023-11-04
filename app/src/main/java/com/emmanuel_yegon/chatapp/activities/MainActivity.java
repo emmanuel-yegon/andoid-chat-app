2cf17c5d7b78376b2497f35fa20e1d27b22c6a3d
emmanuel-yegon <kptooman@gmail.com>
2023-11-04 14:46:47 +0300

    implemented sign-up functionality;store data in firestore db,validated input fields

--- app/src/main/java/com/emmanuel_yegon/chatapp/activities/MainActivity.java
+++ app/src/main/java/com/emmanuel_yegon/chatapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.emmanuel_yegon.chatapp;
+package com.emmanuel_yegon.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.emmanuel_yegon.chatapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
